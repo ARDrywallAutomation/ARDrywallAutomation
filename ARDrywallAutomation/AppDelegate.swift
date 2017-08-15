a3de6a8072f5b333e5f82a9c8a2135df6ec10aad
Adam Baca <baca.adam@me.com>
2017-08-14 21:59:44 -0600

    Initial Commit

---
+++ ARDrywallAutomation/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ARDrywallAutomation
+//
+//  Created by Adam Baca on 08/14/17
+//  Copyright © ARDrywallAutomation. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
